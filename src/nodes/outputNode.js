43910a443fb28391ad8129d6827e0047f7f00be9
Aakash <hello@aakash.design>
2026-06-04 15:26:00 +0530

    Refactor InputNode and OutputNode to utilize BaseNode component for better structure and consistency in handling state changes.

--- src/nodes/outputNode.js
+++ src/nodes/outputNode.js
@@ -1,22 +1,47 @@
 // outputNode.js
 
 import { useState } from 'react';
-import { Handle, Position } from 'reactflow';
+import { Position } from 'reactflow';
+import { BaseNode } from './BaseNode';
 
 export const OutputNode = ({ id, data }) => {
-  const [currName, setCurrName] = useState(data?.outputName || id.replace('customOutput-', 'output_'));
-  const [outputType, setOutputType] = useState(data.outputType || 'Text');
-
-  const handleNameChange = (e) => {
-    setCurrName(e.target.value);
-  };
-
-  const handleTypeChange = (e) => {
-    setOutputType(e.target.value);
-  };
+  const [name, setName] = useState(data?.outputName || id.replace('customOutput-', 'output_'));
+  const [type, setType] = useState(data.outputType || 'Text');
 
   return (
-    <div style={{width: 200, height: 80, border: '1px solid black'}}>
+    <BaseNode
+      type="output"
+      handles={[
+        { id: `${id}-value`, type: 'target', position: Position.Left }
+      ]}
+    >
+      <div>
+        <span>Output</span>
+      </div>
+      <div>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label>
+          Type:
+          <select value={type} onChange={(e) => setType(e.target.value)}>
+            <option value="Text">Text</option>
+            <option value="File">Image</option>
+          </select>
+        </label>
+      </div>
+    
+    </BaseNode>
+  );
+}
+
+/*
+<div style={{width: 200, height: 80, border: '1px solid black'}}>
       <Handle
         type="target"
         position={Position.Left}
@@ -43,5 +68,5 @@ export const OutputNode = ({ id, data }) => {
         </label>
       </div>
     </div>
-  );
-}
+
+*/
