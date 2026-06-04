9aa3e4762f037c53c9ad3c3a4bb40d069853ce0c
Aakash <hello@aakash.design>
2026-06-04 15:43:48 +0530

    updated text node

--- src/nodes/textNode.js
+++ src/nodes/textNode.js
@@ -1,17 +1,41 @@
 // textNode.js
 
 import { useState } from 'react';
-import { Handle, Position } from 'reactflow';
+import { Position } from 'reactflow';
+import { BaseNode } from './BaseNode';
 
 export const TextNode = ({ id, data }) => {
-  const [currText, setCurrText] = useState(data?.text || '{{input}}');
+  const [text, setText] = useState(data?.text || '{{input}}');
 
-  const handleTextChange = (e) => {
-    setCurrText(e.target.value);
-  };
 
   return (
-    <div style={{width: 200, height: 80, border: '1px solid black'}}>
+   <BaseNode
+    type="output"
+    handles={[
+      { id: `${id}-output`, type: 'source', position: Position.Right }
+    ]}
+    >
+    <div>
+        <span>Text</span>
+      </div>
+      <div>
+        <label>
+          Text:
+          <input 
+            type="text" 
+            value={text} 
+            onChange={(e) => setText(e.target.value)}
+          />
+        </label>
+      </div>
+    </BaseNode>
+  );
+}
+
+
+/*
+
+ <div style={{width: 200, height: 80, border: '1px solid black'}}>
       <div>
         <span>Text</span>
       </div>
@@ -31,5 +55,6 @@ export const TextNode = ({ id, data }) => {
         id={`${id}-output`}
       />
     </div>
-  );
-}
+
+*/
+
